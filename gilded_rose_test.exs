11b9be78fa0fb43d89b9be41798155a4e24345ed
Matthew Boehlig <thetamind@gmail.com>
2017-09-09 01:32:11 -0500

    Test special items

--- gilded_rose_test.exs
+++ gilded_rose_test.exs
@@ -14,29 +14,83 @@ defmodule GildedRoseTest do
     assert GildedRose.Driver.run(30) == expected
   end
 
-  describe "quality" do
-    test "degrades by one" do
+  describe "generic item" do
+    test "quality degrades by one" do
       item = generic_item(quality: 20)
       next_item = GildedRose.update_item(item)
       assert next_item.quality == 19
     end
 
-    test "degrades by two after sell by date" do
+    test "quality degrades by two after sell by date" do
       item = generic_item(sell_in: -5, quality: 20)
       next_item = GildedRose.update_item(item)
       assert next_item.quality == 18
     end
 
-    test "never becomes negative" do
+    test "quality never becomes negative" do
       item = generic_item(quality: 0)
       next_item = GildedRose.update_item(item)
       assert next_item.quality == 0
     end
 
-    test "never becomes more than 50" do
+    test "quality never becomes more than 50" do
       item = aged_brie(quality: 50)
       next_item = GildedRose.update_item(item)
       assert next_item.quality == 50
+    end
+  end
+
+  describe "Aged Brie" do
+    test "quality increases" do
+      item = aged_brie(quality: 30)
+      next_item = GildedRose.update_item(item)
+      assert next_item.quality == 31
+    end
+
+    test "quality increases by two after sell date" do
+      item = aged_brie(sell_in: -5, quality: 30)
+      next_item = GildedRose.update_item(item)
+      assert next_item.quality == 32
+    end
+  end
+
+  describe "Sulfuras" do
+    test "quality is always 80" do
+      item = sulfuras()
+      next_item = GildedRose.update_item(item)
+      assert next_item.quality == 80
+    end
+
+    test "sell date never changes" do
+      item = sulfuras(sell_in: 10)
+      next_item = GildedRose.update_item(item)
+      assert next_item.sell_in == 10
+    end
+  end
+
+  describe "Backstage Passes" do
+    test "quality increases" do
+      item = backstage_passes(sell_in: 15, quality: 30)
+      next_item = GildedRose.update_item(item)
+      assert next_item.quality == 31
+    end
+
+    test "quality increases by 2 when 10 days or less" do
+      item = backstage_passes(sell_in: 10, quality: 30)
+      next_item = GildedRose.update_item(item)
+      assert next_item.quality == 32
+    end
+
+    test "quality increases by 3 when 5 days or less" do
+      item = backstage_passes(sell_in: 5, quality: 30)
+      next_item = GildedRose.update_item(item)
+      assert next_item.quality == 33
+    end
+
+    test "quality is 0 when expired" do
+      item = backstage_passes(sell_in: 0, quality: 30)
+      next_item = GildedRose.update_item(item)
+      assert next_item.quality == 0
     end
   end
 
@@ -50,5 +104,16 @@ defmodule GildedRoseTest do
 
   defp aged_brie(opts) do
     generic_item(Keyword.put(opts, :name, "Aged Brie"))
+  end
+
+  defp sulfuras(opts \\ []) do
+    opts
+    |> Keyword.put(:name, "Sulfuras, Hand of Ragnaros")
+    |> Keyword.put(:quality, 80)
+    |> generic_item()
+  end
+
+  defp backstage_passes(opts) do
+    generic_item(Keyword.put(opts, :name, "Backstage passes to a TAFKAL80ETC concert"))
   end
 end
