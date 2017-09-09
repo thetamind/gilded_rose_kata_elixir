f23f3f2386a24aa6ca378e8e30ce412c5c3be954
Matthew Boehlig <thetamind@gmail.com>
2017-09-09 02:25:39 -0500

    Extract sulfuras to function heads

--- gilded_rose.ex
+++ gilded_rose.ex
@@ -19,15 +19,15 @@ defmodule GildedRose do
     %Item{item | sell_in: sell_in - 1, quality: min(item.quality + 1, 50)}
   end
 
+  def update_item(%Item{name: "Sulfuras, Hand of Ragnaros"} = item) do
+    item
+  end
+
   def update_item(item) do
     item = cond do
       item.name != "Backstage passes to a TAFKAL80ETC concert" ->
         if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            %{item | quality: item.quality - 1}
-          else
-            item
-          end
+          %{item | quality: item.quality - 1}
         else
           item
         end
@@ -60,22 +60,16 @@ defmodule GildedRose do
           true -> item
         end
     end
-    item = cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        %{item | sell_in: item.sell_in - 1}
-      true -> item
-    end
+
+    item = %{item | sell_in: item.sell_in - 1}
+
     cond do
       item.sell_in < 0 ->
         cond do
           item.name != "Backstage passes to a TAFKAL80ETC concert" ->
             cond do
               item.quality > 0 ->
-                cond do
-                  item.name != "Sulfuras, Hand of Ragnaros" ->
-                    %{item | quality: item.quality - 1}
-                  true -> item
-                end
+                %{item | quality: item.quality - 1}
               true -> item
             end
           true -> %{item | quality: item.quality - item.quality}
